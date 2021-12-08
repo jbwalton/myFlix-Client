f70e1f4aedfc96a4ec20a65b0cacdc3308a3feb1
jbwalton <johngray3d@googlemail.com>
2021-12-08 10:57:32 +0100

    Create movie-view.jsx
    updated movie view

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./movie-view.scss"
+
+export class MovieView extends React.Component {
+
+  keypressCallback(event) {
+    console.log(event.key);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keypress', this.keypressCallback);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.keypressCallback);
+  }
+  
+  render() {
+    const { movie, onBackClick } = this.props;
+
+    return (
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.ImagePath} crossOrigin="true" />
+        </div>
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <div className="director-bio">
+          <span className="director">Bio: </span>
+          <span className="value">{movie.Director.Bio}</span>
+        </div>
+        <div className="director-birth">
+          <span className="director">Birth: </span>
+          <span className="value">{movie.Director.Birth}</span>
+        </div>
+        <div className="director-death">
+          <span className="director">Death: </span>
+          <span className="value">{movie.Director.Death}</span>
+        </div>
+        <div className="movie-actors">
+          <span className="value">{movie.Actors}</span>
+        </div>
+        <button onClick={() => { onBackClick(null); }}>Back</button>
+
+      </div>
+    );
+  }
+}
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.date,
+      Death: PropTypes.date
+    }),
+    Featured: PropTypes.bool,
+    ImagePath: PropTypes.string.isRequired
+    }).isRequired,
+    onBackClick: PropTypes.func.isRequired
+};
