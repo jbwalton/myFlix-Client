7827b71b23f59f224604711eb5e799f9f2dfbd50
jbwalton <johngray3d@googlemail.com>
2021-12-08 10:57:00 +0100

    Create movie-card.jsx
    updated movie card view

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import "./movie-card.scss"
+
+// React Bootstrap
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export class MovieCard extends React.Component {
+  render() {
+    const { movie, onMovieClick } = this.props;
+
+    return (
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.date,
+      Death: PropTypes.date
+    }),
+    Featured: PropTypes.bool,
+    ImagePath: PropTypes.string.isRequired,
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
