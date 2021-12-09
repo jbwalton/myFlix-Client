a9dc7000d774325ec702b9aa01dccff8df8e40c8
jbwalton <johngray3d@googlemail.com>
2021-12-09 16:39:20 +0100

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -3,17 +3,17 @@ import axios from 'axios';
 import React, { useState } from 'react';
 
 // React Bootstrap
-import { Form, Button, Container } from 'react-bootstrap';
+import { Form, Button, Container, Col, Row, CardGroup, Card } from 'react-bootstrap';
 import "./registration-view.scss"
 export function RegistrationView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ email, setEmail ] = useState('');
-  const [ birthday, setBirthday ] = useState('');
+  const [ birthday, setBirth_Date ] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('https://movieapi-morkos.herokuapp.com/users', {
+    axios.post('https://quiet-taiga-13679.herokuapp.com/users', {
       Username: username,
       Password: password,
       Email: email,
@@ -29,26 +29,44 @@ export function RegistrationView(props) {
     });
 };
   return (
-    <Container>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+    <Container fluid className="registerContainer">
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card className="registerCard">
+              <Card.Body>
+                <Card.Title className="text-center">Welcome to myMovies.</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted text-center">Please Register</Card.Subtitle>
+            
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Birthday</Form.Label>
+                    <Form.Control className="mb-3" type="date" value={birthday} onChange={e => setBirth_Date(e.target.value)} />
+                  </Form.Group>
+                  
+                  <Button className="registerButton" variant="secondary" size="lg" type="submit" onClick={handleSubmit}>Register</Button>
+                  
+                </Form>
+              </Card.Body>
+            </Card>
+        </CardGroup>
+        </Col>
+      </Row>
     </Container>
   );
 }
