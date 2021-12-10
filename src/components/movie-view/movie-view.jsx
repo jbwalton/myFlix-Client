04483a77b8ab6284032939e6c3a0cce58cfafe11
jbwalton <johngray3d@googlemail.com>
2021-12-10 12:34:37 +0100

    Update movie-view.jsx
    updated movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import "./movie-view.scss"
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Col, Container, Button, Form, CardGroup, Card } from 'react-bootstrap';
 export class MovieView extends React.Component {
 
   keypressCallback(event) {
@@ -21,46 +21,71 @@ export class MovieView extends React.Component {
 
     return (
       <Container fluid className="moviesContainer">
-      <Row>
-      <Col>
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} crossOrigin="true" />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <div className="director-bio">
-          <span className="director">Bio: </span>
-          <span className="value">{movie.Director.Bio}</span>
-        </div>
-        <div className="director-birth">
-          <span className="director">Birth: </span>
-          <span className="value">{movie.Director.Birth}</span>
-        </div>
-        <div className="director-death">
-          <span className="director">Death: </span>
-          <span className="value">{movie.Director.Death}</span>
-        </div>
-        <div className="movie-button-div">
-          <button className="movie-button" onClick={() => { onBackClick(null); }}>Back</button>
-        </div>
-      </div>
-      </Col>
-      </Row>
+        <Row>
+          <Col>
+            <div className="movie-view">
+              <div className="movie-poster">
+                <img src={movie.ImagePath} crossOrigin="true" />
+              </div>
+              <Card>
+              <Card.Body>
+                  <Form.Group>
+                    <Card.Title>{movie.Title}</Card.Title>
+                  </Form.Group>
+              
+              <Form.Group>
+                <Form.Label>Genre: </Form.Label>
+                <div>
+                 <span className="value">{movie.Genre.Name}</span>
+                </div>
+              </Form.Group>
+              <br></br>
+              
+              
+              <Form.Group>
+                <Form.Label>Description: </Form.Label>
+                <div>
+                <span className="value">{movie.Description}</span>
+                </div>
+              </Form.Group>
+              <br></br>
+              <Form.Group>
+                <Form.Label>Director: </Form.Label>
+                <div>
+                <span className="value">{movie.Director.Name}</span>
+                </div>
+              </Form.Group>
+              <br></br>
+              <Form.Group>
+                <Form.Label>Bio: </Form.Label>
+                <div>
+                <span className="value">{movie.Director.Bio}</span>
+                </div>
+              </Form.Group>
+              <br></br>
+              <Form.Group>
+                <Form.Label>Birth: </Form.Label>
+                <div>
+                <span className="value">{movie.Director.Birth}</span>
+                </div>
+              </Form.Group>
+              <br></br>             
+              <Form.Group>
+                <Form.Label>Death: </Form.Label>
+                <div>
+                <span className="value">{movie.Director.Death}</span>
+                </div>
+              </Form.Group>
+              <br></br>  
+              
+              </Card.Body>
+              </Card>
+              <div className="movie-button-div">
+                <Button variant="primary" onClick={() => { onBackClick(null); }}>Back</Button>
+              </div>
+            </div>
+          </Col>
+        </Row>
       </Container>
     );
   }
